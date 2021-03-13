6716b67c80e6d91219e92e4cf382334ac7924673
Samuel Akabo <superakabo@gmail.com>
2021-03-13 08:06:25 +0000

    successfully ported PhoneNumberMatch.dart

---
+++ lib/i18n/phonenumbers/PhoneNumberMatch.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'generated/phonenumber.pb.dart';
+
+class PhoneNumberMatch {
+  final int _start;
+  final String _rawString;
+  final PhoneNumber _number;
+
+  PhoneNumberMatch(
+    this._start,
+    this._rawString,
+    this._number,
+  ) : assert(_start > -1, 'Start index must be >= 0');
+
+  PhoneNumber get number => _number;
+
+  int get start => _start;
+
+  int get end => (_start + _rawString.length);
+
+  String get rawString => _rawString;
+
+  @override
+  int get hashCode => hashValues(start, rawString, number);
+
+  bool equals(Object obj) {
+    if (this == obj) return true;
+
+    if (!(obj is PhoneNumberMatch)) return false;
+
+    PhoneNumberMatch other = obj;
+
+    return ((rawString == other.rawString) && (start == other.start) && (number == other.number));
+  }
+
+  @override
+  String toString() {
+    return "PhoneNumberMatch [$start,$end)$rawString";
+  }
+}
---
+++ lib/i18n/phonenumbers/PhoneNumberMatcher.dart
@@ -0,0 +1,11 @@
+// class PhoneNumberMatcher implements Iterator<PhoneNumberMatch> {
+//   @override
+//   // TODO: implement current
+//   get current => throw UnimplementedError();
+
+//   @override
+//   bool moveNext() {
+//     // TODO: implement moveNext
+//     throw UnimplementedError();
+//   }
+// }
